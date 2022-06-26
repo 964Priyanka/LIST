e12c642d2dd5ea994c285ae86f943233227c23a7
964Priyanka <94621133+964Priyanka@users.noreply.github.com>
2022-06-26 22:08:35 +0530

    https://github.com/964Priyanka/LIST.git

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-
-import './App.css';
-import Todolist from "./Todolist";
+import { Toaster } from 'react-hot-toast';
+import AppContent from './components/AppContent';
+import AppHeader from './components/AppHeader';
+import PageTitle from './components/PageTitle';
+import todoList from './TodoList.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Todo list</h1>
-      <Todolist/>
-    </div>
-    
+    <>
+      <div className="container">
+        <PageTitle>TODO List</PageTitle>
+        <div className={styles.app__wrapper}>
+          <AppHeader />
+          <AppContent />
+        </div>
+      </div>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            fontSize: '1.4rem',
+          },
+        }}
+      />
+    </>
   );
 }
 
